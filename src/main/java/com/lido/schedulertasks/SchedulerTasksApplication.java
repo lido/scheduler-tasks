270ab2dbd85a6aad1361cc18b28907f5a0e3823f
Lidoan <lidoanpinto@gmail.com>
2025-05-12 11:07:25 -0300

    Initial commit

---
+++ src/main/java/com/lido/schedulertasks/SchedulerTasksApplication.java
@@ -0,0 +1,13 @@
+package com.lido.schedulertasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lido/schedulertasks/SchedulerTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lido.schedulertasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
