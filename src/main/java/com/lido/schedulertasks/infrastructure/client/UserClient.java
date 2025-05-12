ed8f6a87c20c8abb74c91f973d984e61f85fe490
Lidoan <lidoanpinto@gmail.com>
2025-05-12 11:59:22 -0300

    added config security

---
+++ src/main/java/com/lido/schedulertasks/infrastructure/client/UserClient.java
@@ -0,0 +1,14 @@
+package com.lido.schedulertasks.infrastructure.client;
+
+import com.lido.schedulertasks.business.dto.UserDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "user", url = "${user.url}")
+public interface UserClient {
+
+    @GetMapping
+    UserDTO findUserByEmail(@RequestParam("email") String email,
+                            @RequestParam("Authorization") String token);
+}
